e5a901a8caf0d569ee41160647bd03c3211bd59f
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2024-02-16 10:48:24 +0000

    Rename dotnet option

--- Bitmagic.VscExtension/src/constants.ts
+++ Bitmagic.VscExtension/src/constants.ts
@@ -4,7 +4,7 @@ export default class Constants
     public static readonly SettingsAutoUpdate = 'bitMagic.debugger.autoUpdateDebugger';
     public static readonly SettingsAlternativeDebugger = 'bitMagic.debugger.alternativePath';
     public static readonly SettingsUseDevelop = "bitMagic.debugger.developRelease";
-    public static readonly SettingsUseOwnDotnet = "bitMagic.debugger.useOwnDotnet";
+    public static readonly SettingsUseOwnDotnet = "bitMagic.debugger.useBundledDotnet";
     public static readonly SettingsDownloadEmulator = 'bitMagic.officialEmulator.downloadOfficialEmulator';
     public static readonly SettingsEmulatorLocation = 'bitMagic.officialEmulator.officialEmulatorLocation';
     public static readonly SettingsEmulatorVersion = 'bitMagic.officialEmulator.version';
